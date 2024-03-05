01b821475adb7c918111ffedbc3b8db9d513aeae
moon <shawmakesmagic@gmail.com>
2024-03-04 20:28:31 -0800

    improve exports

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
 // Export from ./src/lib/actions
 export {
-  defaultActions,
   composeActionExamples,
-  getFormattedActions,
+  defaultActions,
+  formatActionConditions,
   formatActionNames,
   formatActions,
-  formatActionConditions,
+  getFormattedActions,
 } from "./lib/actions";
 
 // Export from ./src/lib/constants
 export { zeroUuid, zeroUuidPlus1 } from "./lib/constants";
 
 // Export from ./src/lib/context
-export { composeContext, addHeader } from "./lib/context";
+export { addHeader, composeContext } from "./lib/context";
 
 // Export from ./src/lib/evaluators
 export {
   defaultEvaluators,
   evaluationTemplate,
-  formatEvaluatorNames,
-  formatEvaluators,
   formatEvaluatorConditions,
-  formatEvaluatorExamples,
   formatEvaluatorExampleConditions,
   formatEvaluatorExampleDescriptions,
+  formatEvaluatorExamples,
+  formatEvaluatorNames,
+  formatEvaluators,
 } from "./lib/evaluators";
 
 // Export from ./src/lib/goals
 export {
-  getGoals,
-  formatGoalsAsString,
-  updateGoal,
-  createGoal,
   cancelGoal,
+  createGoal,
   finishGoal,
   finishGoalObjective,
+  formatGoalsAsString,
+  getGoals,
+  updateGoal,
 } from "./lib/goals";
 
 // Export from ./src/lib/lore
-export { addLore, getLore, formatLore } from "./lib/lore";
+export { addLore, formatLore, getLore } from "./lib/lore";
 
 // Export from ./src/lib/memory
 export {
@@ -48,7 +48,7 @@ export {
 } from "./lib/memory";
 
 // Export from ./src/lib/messages
-export { getActorDetails, formatActors, formatMessages } from "./lib/messages";
+export { formatActors, formatMessages, getActorDetails } from "./lib/messages";
 
 // Export from ./src/lib/providers
 export { defaultProviders, getProviders } from "./lib/providers";
@@ -56,9 +56,9 @@ export { defaultProviders, getProviders } from "./lib/providers";
 // Export from ./src/lib/relationships
 export {
   createRelationship,
+  formatRelationships,
   getRelationship,
   getRelationships,
-  formatRelationships,
 } from "./lib/relationships";
 
 // Export from ./src/lib/runtime
@@ -70,117 +70,33 @@ export { messageHandlerTemplate } from "./lib/templates";
 // Export from ./src/lib/types
 export { GoalStatus } from "./lib/types";
 export type {
-  Content,
-  ActionExample,
-  ConversationExample,
-  Actor,
-  Memory,
-  Objective,
-  Goal,
-  State,
-  Message,
-  MessageExample,
-  Handler,
-  Validator,
   Action,
+  ActionExample,
+  Actor,
+  Content,
+  ConversationExample,
   EvaluationExample,
   Evaluator,
+  Goal,
+  Handler,
+  Memory,
+  Message,
+  MessageExample,
+  Objective,
   Provider,
   Relationship,
+  State,
+  Validator,
 } from "./lib/types";
 
 // Export from ./src/lib/utils
-export { parseJsonArrayFromText, parseJSONObjectFromText } from "./lib/utils";
-
-// Export from ./src/lib/actions/continue
-export { default as continueAction } from "./lib/actions/continue";
-
-// Export from ./src/lib/actions/ignore
-export { default as ignoreAction } from "./lib/actions/ignore";
-
-// Export from ./src/lib/actions/wait
-export { default as waitAction } from "./lib/actions/wait";
-
-// Export from ./src/lib/evaluators/fact
-export { formatFacts, default as factEvaluator } from "./lib/evaluators/fact";
-
-// Export from ./src/lib/evaluators/goal
-export { default as goalEvaluator } from "./lib/evaluators/goal";
-
-// Export from ./src/lib/providers/time
-export { default as timeProvider } from "./lib/providers/time";
-
-// Export from ./src/lib/logger
-export { default as logger } from "./lib/logger";
-
-// Export from ./src/test/cache
-import { getCachedEmbedding, writeCachedEmbedding } from "./test/cache";
-
-// Export from ./src/test/constants
-import {
-  SERVER_URL,
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY,
-  TEST_EMAIL,
-  TEST_PASSWORD,
-  TEST_EMAIL_2,
-  TEST_PASSWORD_2,
-} from "./test/constants";
+export { parseJSONObjectFromText, parseJsonArrayFromText } from "./lib/utils";
 
 // Export from ./src/test/createRuntime
-import { createRuntime } from "./test/createRuntime";
-
-// Export from ./src/test/data
-import {
-  GetTellMeAboutYourselfConversation1,
-  GetTellMeAboutYourselfConversation2,
-  GetTellMeAboutYourselfConversation3,
-  GetTellMeAboutYourselfConversationTroll1,
-  GetTellMeAboutYourselfConversationTroll2,
-  Goodbye1,
-  jimFacts,
-} from "./test/data";
+export { createRuntime } from "./test/createRuntime";
 
 // Export from ./src/test/populateMemories
-import { populateMemories } from "./test/populateMemories";
-
-// Export from ./src/test/report
-import { deleteReport, addToReport, logReport } from "./test/report";
+export { populateMemories } from "./test/populateMemories";
 
 // Export from ./src/test/runAiTest
-import { runAiTest } from "./test/runAiTest";
-
-// Export from ./src/test/testAction
-import { TEST_ACTION, TEST_ACTION_FAIL } from "./test/testAction";
-
-// Export from ./src/test/testEvaluator
-import { TEST_EVALUATOR, TEST_EVALUATOR_FAIL } from "./test/testEvaluator";
-
-export const test = {
-  getCachedEmbedding,
-  writeCachedEmbedding,
-  SERVER_URL,
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY,
-  TEST_EMAIL,
-  TEST_PASSWORD,
-  TEST_EMAIL_2,
-  TEST_PASSWORD_2,
-  createRuntime,
-  GetTellMeAboutYourselfConversation1,
-  GetTellMeAboutYourselfConversation2,
-  GetTellMeAboutYourselfConversation3,
-  GetTellMeAboutYourselfConversationTroll1,
-  GetTellMeAboutYourselfConversationTroll2,
-  Goodbye1,
-  jimFacts,
-  populateMemories,
-  deleteReport,
-  addToReport,
-  logReport,
-  runAiTest,
-  TEST_ACTION,
-  TEST_ACTION_FAIL,
-  TEST_EVALUATOR,
-  TEST_EVALUATOR_FAIL,
-};
+export { runAiTest } from "./test/runAiTest";
